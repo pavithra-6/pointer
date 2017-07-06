e9f54bcc6834431ab60d54402a8e0d5d494f835a
pavithra-6 <paviikannan1996@gmail.com>
2017-07-06 10:31:56 +0530

    Add files via upload

---
+++ Multable.java
@@ -0,0 +1,15 @@
+package guvi;
+import java.util.Scanner;
+public class Multable {
+
+		public static void main(String[] args) 
+	    {
+	        Scanner s = new Scanner(System.in);
+		System.out.print("Enter number:");        
+		int n=s.nextInt();
+	        for(int i=1; i <= 10; i++)
+	        {
+	            System.out.println(n+" * "+i+" = "+n*i);
+	        }
+	    }
+	}
